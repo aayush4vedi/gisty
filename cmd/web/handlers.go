da31cd65c658b9f1680ddcf406875bd9778fc2c9
4vediaayush <aayush.chaturvedi@razorpay.com>
2020-02-10 13:03:40 +0530

    #3.5 Executing SQL Statements

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,7 +9,7 @@ import (
 
 func (app *App) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		app.notFound(w)	// Use the notFound() helper 
+		app.notFound(w) // Use the notFound() helper
 		return
 	}
 	files := []string{
@@ -19,13 +19,13 @@ func (app *App) home(w http.ResponseWriter, r *http.Request) {
 	}
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.serverError(w,err)  // Use the serverError() helper. 
+		app.serverError(w, err) // Use the serverError() helper.
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 	err = ts.Execute(w, nil)
 	if err != nil {
-		app.serverError(w,err)  // Use the serverError() helper. 
+		app.serverError(w, err) // Use the serverError() helper.
 		http.Error(w, "Internal Server Error", 500)
 	}
 }
@@ -33,7 +33,7 @@ func (app *App) home(w http.ResponseWriter, r *http.Request) {
 func (app *App) showGist(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		app.notFound(w)	// Use the notFound() helper 
+		app.notFound(w) // Use the notFound() helper
 		return
 	}
 	fmt.Fprintf(w, "Display a specific gist with ID %d..", id)
@@ -43,8 +43,21 @@ func (app *App) showGist(w http.ResponseWriter, r *http.Request) {
 func (app *App) createGist(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		w.Header().Set("Allow", "POST")
-		app.clientError(w, http.StatusMethodNotAllowed)   //use clientError
+		app.clientError(w, http.StatusMethodNotAllowed) //use clientError
 		return
 	}
-	w.Write([]byte("Create a new gist..."))
+
+	// Create some variables holding dummy data
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi"
+	expires := "7"
+
+	// Pass the data to the GistModel.Insert() method, receiving the ID of the new record back.
+	id, err := app.gists.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	// Redirect the user to the relevant page for the snippet.
+	http.Redirect(w, r, fmt.Sprintf("/gist?id=%d", id), http.StatusSeeOther)
 }
--- pkg/models/mysql/gisty.go
+++ pkg/models/mysql/gisty.go
@@ -11,17 +11,34 @@ type GistModel struct {
 	DB *sql.DB
 }
 
-// This will insert a new snippet into the database.
 func (m *GistModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	// Write the SQL statement we want to execute.
+	stmt := `INSERT INTO gisty (title, content, created, expires)
+	VALUES (?, ?, date(now()), adddate(now(), ?))`
+
+	// Use the Exec() method on the embedded connection pool to execute the
+	// statement. This method returns a sql.Result object, which contains some basic
+	// information about what happened when the statement was execute
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	// Use the LastInsertId() method on the result object to get the ID of our
+	// newly inserted record in the snippets table.
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	// The ID returned has the type int64, so we convert it to an int type before returning
+	return int(id), nil
 }
 
-// This will return a specific snippet based on its id.
 func (m *GistModel) Get(id int) (*models.Gist, error) {
 	return nil, nil
 }
 
-// This will return the 10 most recently created snippets.
 func (m *GistModel) Latest() ([]*models.Gist, error) {
 	return nil, nil
 }
